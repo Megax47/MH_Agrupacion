db69ce6f486390dbe0de99c0af29fb9d02466831
Megax47 <aitordelaiglesia@gmail.com>
2026-04-27 19:11:11 +0200

    select, optimice AGG,

--- inc/genetic.h
+++ inc/genetic.h
@@ -2,7 +2,7 @@
 
 #include <mh.h>
 #include <problem.h>
-#include <set>
+#include <vector>
 
 struct Cromosoma {
     tSolution<int> genes;
@@ -18,26 +18,65 @@ struct Cromosoma {
 class Genetic : public MH<int> {
 
 protected:
+    std::vector<Cromosoma> population;
     const int population_size = 50;
-    const int tournament_size = 5;
+    const int tournament_size = 3;
     const float mutation_rate = 0.01;
     const float crossover_rate = 0.8;
-
+    
 public:
+    Genetic();
     ResultMH<int> optimize(Problem<int> &problem, int maxevals);
-    Cromosoma select();
+    int select();
     std::pair<Cromosoma, Cromosoma> crossover(const Cromosoma &parent1, const Cromosoma &parent2);
     void mutate(Cromosoma &solution, Problem<int> &problem);
+
+protected:
+    int bestFit(){
+        int b = -1;
+        float bf = MAXFLOAT;
+        for(int i=0; i < population_size; ++i){
+            if(population[i].fitness < bf){
+                bf = population[i].fitness;
+                b = i;
+            }
+        }
+    }
+    
+    //Primero es el peor, después el segundo peor
+    std::pair<int,int> lessFit() { 
+        std::pair<int,int> w;
+        w.first = -1;
+        w.second = -1;
+        float wf = 0;
+        float wf2 = 0;
+        for(int i=0; i<population_size; ++i){
+            if(population[i].fitness > wf2){
+                if(population[i].fitness > wf){
+                    wf2 = wf;
+                    w.second = w.first;
+                    wf = population[i].fitness;
+                    w.first = i;
+                }
+                else{
+                    wf2 = population[i].fitness;
+                    w.second = i;
+                }
+            }
+        }
+    }
 };
 
 class AGG : public Genetic {
     ResultMH<int> optimize(Problem<int> &problem, int maxevals);
 };
 
+class AGG_UN : public AGG {
+    
+};
+
 class AGE : public Genetic {
 
-protected:
-    std::set<Cromosoma> population;
 public:
     ResultMH<int> optimize(Problem<int> &problem, int maxevals);
 };
--- src/genetic.cpp
+++ src/genetic.cpp
@@ -1,5 +1,31 @@
 #include <genetic.h>
 
+int Genetic::select(){
+    std::vector<int> index(population_size);
+    std::iota(std::begin(index), std::end(index), 0); 
+    Random::shuffle(index);
+
+    float bf = MAXFLOAT;
+    int best = -1;
+
+    for(int i=0; i<tournament_size; ++i){
+        if(population[index[i]].fitness < bf){
+            bf = population[index[i]].fitness;
+            best = i;
+        }
+    }
+
+    return best;
+}
+
+void Genetic::mutate(Cromosoma &solution, Problem<int> &problem){
+    do{ //?Si veo que puede atascarse, evitar repeticiones con un shufle
+        int pos = Random::get<int>(0,problem.getSolutionSize());
+        int val = Random::get<int>(0,problem.getSolutionDomainRange().second);
+        solution.genes[pos] = val;
+    }while(!problem.isValid(solution.genes));
+}
+
 ResultMH<int> AGE::optimize(Problem<int> &problem, int maxevals) {
     int evaluations = population_size;
 
@@ -7,37 +33,112 @@ ResultMH<int> AGE::optimize(Problem<int> &problem, int maxevals) {
     for (int i = 0; i < population_size; ++i) {
         tSolution<int> solution = problem.createSolution();
         tFitness fitness = problem.fitness(solution);
-        population.emplace(solution, fitness);
+        population.emplace_back(solution, fitness);
     }
 
     while (evaluations < maxevals) {
         // Seleccionar padres
-        Cromosoma parent1 = select();
-        Cromosoma parent2 = select();
+        Cromosoma parent1 = population[select()];
+        Cromosoma parent2 = population[select()];
 
         // Cruzar padres para generar hijos
         auto [child1, child2] = crossover(parent1, parent2);
 
-        // Mutar hijos
-        mutate(child1, problem);
-        mutate(child2, problem);
+        // Mutar hijos con cierta probabilidad
+        if(Random::get<bool>(mutation_rate)) mutate(child1, problem);
+        if(Random::get<bool>(mutation_rate)) mutate(child2, problem);
 
         // Evaluar hijos
         tFitness fitness1 = problem.fitness(child1.genes);
         tFitness fitness2 = problem.fitness(child2.genes);
         evaluations += 2;
 
-        //?MEJORABLE
-        // Agregar hijos a la población
-        population.emplace(child1,fitness1);
-        population.emplace(child2,fitness2);
+        child1.fitness = fitness1;
+        child2.fitness = fitness2;
 
-        //Eliminamos los dos peores
-        population.erase(*population.rbegin());
-        population.erase(*population.rbegin());
+        std::pair<int,int> worst = lessFit();
+        float wf1 = population[worst.first].fitness;
+        float wf2 = population[worst.second].fitness;
 
+        if(fitness1 < fitness2){
+            if(fitness2 < wf2){
+                population[worst.first] = child1;
+                population[worst.second] = child2;
+            }else if(fitness1 < wf2){
+                population[worst.first] = child1;
+            }else if(fitness1 < wf1){
+                population[worst.first] = child1;
+            }
+        }else{
+            if(fitness1 < wf2){
+                population[worst.first] = child2;
+                population[worst.second] = child1;
+            }else if(fitness2 < wf2){
+                population[worst.first] = child2;
+            }else if(fitness2 < wf1){
+                population[worst.first] = child2;
+            }
+        }
     }
-
-    Cromosoma best = *population.begin();
+    Cromosoma best = population[bestFit()];
     return ResultMH(best.genes, best.fitness, evaluations); // Devolver el mejor individuo encontrado
 }
+
+ResultMH<int> AGG::optimize(Problem<int> &problem, int maxevals) {
+    int evaluations = population_size;
+
+    // Inicializar la población
+    for (int i = 0; i < population_size; ++i) {
+        tSolution<int> solution = problem.createSolution();
+        tFitness fitness = problem.fitness(solution);
+        population.emplace_back(solution, fitness);
+    }
+
+    while(evaluations < maxevals){
+        
+        //Nos quedamos el mejor (Elitismo)
+        Cromosoma best = population[bestFit()];
+
+        //Seleccionar padres
+        std::vector<int> selected(population_size);
+        for (int i=0; i< population_size; ++i) {
+            selected[i] = select();
+        }
+
+        //Cruzar el número esperado de veces en orden fijo
+        int spected_crossovers = (population_size/2)*crossover_rate;
+        for (int i=0; i < spected_crossovers; i+=2){
+            auto [child1, child2] = crossover(population[selected[i]], population[selected[i+1]]);
+            population[i].genes = child1.genes;
+            population[i+1].genes = child2.genes;
+        }
+
+        //Asignar los que no cruzaron directamente
+        for (int i=spected_crossovers*2; i<population_size; ++i){
+            population[i] = population[selected[i]];
+        }
+
+        //Seleccionamos los cromosomas a mutar
+        std::vector<int> index(population_size);
+        std::iota(std::begin(index), std::end(index), 0); 
+        Random::shuffle(index);
+
+        int spected_mutations = population_size*mutation_rate;
+        for(int i=0; i<spected_mutations; ++i){
+            mutate(population[index[i]], problem);
+        }
+
+        //Calculamos el fitness de todos
+        for(int i=0; i<population_size; ++i){
+            population[i].fitness = problem.fitness(population[i].genes);
+            ++evaluations;
+        }
+
+        //Sustituimos el peor de la actual por el mejor de la anterior
+        population[lessFit().first] = best;
+    }
+    Cromosoma best = population[bestFit()];
+    return ResultMH(best.genes, best.fitness, evaluations); // Devolver el mejor individuo encontrado
+}
+
+std::pair<Cromosoma, Cromosoma> crossover(const Cromosoma &parent1, const Cromosoma &parent2);
